26134c69897fa39edc08f83e8a00fe542ae7fbdb
Carlos <carlosbatista108@hotmail.com>
2016-09-21 00:35:22 -0400

    Clase producto

---
+++ java/src/Entidades/Producto.java
@@ -0,0 +1,52 @@
+package Entidades;
+
+/**
+ * Created by carlosb108 on 9/21/16.
+ */
+public class Producto {
+    private Integer id;
+    private String nombre;
+    private Integer cantidad_disponible;
+    private String descripcion;
+
+
+    public producto_venta( private Integer id, private String nombre, private Integer cantidad_disponible, private String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad_disponible = cantidad_disponible;
+        this.descripcion = descripcion;
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCantidad_disponible() {
+        return cantidad_disponible;
+    }
+
+    public void setCantidad_disponible(Integer cantidad_disponible) {
+        this.cantidad_disponible = cantidad_disponible;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
