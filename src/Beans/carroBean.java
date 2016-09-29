c481fbb1f3a8f40d5141f16450af2a9f3ae62fa2
Carlos <carlosbatista108@hotmail.com>
2016-09-29 15:36:30 -0400

    Agregando funcion Clase: -carroBean.java

--- src/Beans/carroBean.java
+++ src/Beans/carroBean.java
@@ -13,7 +13,28 @@ import java.util.ArrayList;
 @SessionScoped
 public class carroBean {
     private ArrayList< carrito_de_venta > elementos = new ArrayList< >( );
+    private ArrayList< carrito_de_venta > temp_for_clean = new ArrayList<>( );
     public void agregar_a_carro( int id, int cantidad ){
         //
     }
+
+
+    private ArrayList< Contacto > contactos = new ArrayList<>( );
+
+    public String eliminar( ){
+        for( carrito_de_venta c : elementos ){
+            if( c.isSelected( ) ){
+                temp_for_clean.add( c );
+            }
+        }
+
+        if( temp_for_clean.size() > 0 ) {
+            elementos.removeAll(temp_for_clean);
+            temp_for_clean.clear();
+            FacesContext.getCurrentInstance().addMessage( null, new FacesMessage( "Producto/s eliminado/s" ) );
+        }
+        return "index";
+    }
+
+
 }
